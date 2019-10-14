9e83bf2ae49e2e5eecdfe43f75f50fa84e193a3d
Adrian-Garcia <agarc5@hotmail.com>
2019-10-14 17:25:41 -0500

    MVP done with 3 endpoints! might work on stretch

---
+++ auth/protected-middleware.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model');
+
+// brought in from user router at the bottome/commented out
+
+module.exports = function protected(req, res, next) {
+  const { username, password } = req.headers;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'You shall not pass!' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
